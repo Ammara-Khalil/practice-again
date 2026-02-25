f7d80f9a831c4cf1e57d2749f1b8db9a9cf3f437
Ammara <you@example.com>
2026-02-25 11:01:37 +0500

    adding file

---
+++ myapp/src/file.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function HelloSheikhu() {
+  return (
+    <div>
+      <h1 style="color:blue;">Hello Sheikhuuuuuuuuuuuu !!!!!!</h1>
+      <p style="color:red;">Tu buddha ha.</p>
+
+
+
+    </div>
+  );
+}
+
+export default HelloSheikhu;
